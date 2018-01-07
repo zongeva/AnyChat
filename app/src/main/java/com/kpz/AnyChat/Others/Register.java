fe779cbcbfa8779bc200eb4966171214e9d481d7
chy72 <kokpinzong@hotmail.com>
2018-01-08 04:01:25 +0800

    add layout

---
+++ app/src/main/java/com/kpz/AnyChat/Others/Register.java
@@ -0,0 +1,17 @@
+package com.kpz.AnyChat.Others;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kpz.AnyChat.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+}
